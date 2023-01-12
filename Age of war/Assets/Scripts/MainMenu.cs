abd0e17993cc1bec4b817d87549c42bc8b8b523a
overcat425 <105467409+overcat425@users.noreply.github.com>
2023-01-12 21:07:13 +0900

    Age Of War Init
    23/01/12
    Age of war initiate

---
+++ Age of war/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject StagePane;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void StartGame()
+    {
+        StagePane.SetActive(true);
+    }
+    public void StageExit()
+    {
+        StagePane.SetActive(false);
+    }
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+                Application.Quit();
+#endif
+    }
+}
